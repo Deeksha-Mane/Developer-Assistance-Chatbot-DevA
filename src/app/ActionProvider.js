10eb15896ad8b715ab256833cbd49cdf470219e2
Deeksha Mane <manediksha264@gmail.com>
2025-01-21 19:37:43 +0530

    update ActionProvider.js

--- src/app/ActionProvider.js
+++ src/app/ActionProvider.js
@@ -45,27 +45,19 @@ class ActionProvider{
 
     timer = ms => new Promise(res=> setTimeout(res,ms));
 
-    // generateResponseMessage = async(userMessage) => {
-    //     const responseFromGPT = await this.callGenAI(userMessage);
-    //     let message;
-    //     let numberNoLines = responseFromGPT.split('\n').length;
-    //     for (let i = 0; i < numberNoLines; i++) {
-    //         const msg = responseFromGPT.split('\n')[i];
-    //         if (msg.length) {
-    //             message=this.createChatBotMessage(msg);
-    //             this.updateChatBotMessage(message);
-    //         }
-    //         await this.timer(1000);
-    //     }
-    // }
-
-    generateResponseMessage = async (userMessage) => {
-        const responseFromOpenAI = await this.callOpenAI(userMessage);
-
-        const message = this.createChatBotMessage(responseFromOpenAI);
-
-        this.updateChatBotState(message);
-    };
+    generateResponseMessage = async(userMessage) => {
+        const responseFromGPT = await this.callGenAI(userMessage);
+        let message;
+        let numberNoLines = responseFromGPT.split('\n').length;
+        for (let i = 0; i < numberNoLines; i++) {
+            const msg = responseFromGPT.split('\n')[i];
+            if (msg.length) {
+                message=this.createChatBotMessage(msg);
+                this.updateChatBotMessage(message);
+            }
+            await this.timer(1000);
+        }
+    }
 
     respond = (message)=>{
         this.generateResponseMessage(message);
